df156ddb316b47a388156a108f8f3cc47f58a383
abdelmounimmassoum <abdel.massoum@gmail.com>
2023-05-23 22:02:40 -0400

    locators added

--- src/web_elements/student_registration.py
+++ src/web_elements/student_registration.py
@@ -31,6 +31,16 @@ print('maximizing the browser window')
 driver.maximize_window()
 time.sleep(1)
 
+# Sample of different locator:
+driver.get('https://demoqa.com/links')
+time.sleep(2)
+driver.find_element(By.LINK_TEXT, 'Created').click()
+time.sleep(5)
+print('Clicking the No Content link based on the partial text')
+driver.find_element(By.PARTIAL_LINK_TEXT, 'Content').click()
+time.sleep(10)
+
+
 print('open the student registration form')
 driver.get(host)
 print('Zoom the page to 75%')
@@ -38,17 +48,30 @@ driver.execute_script("document.body.style.zoom='75%'")
 
 # Enter Mobile, Subjects, current Address
 fname_input = driver.find_element(By.ID, 'firstName')
-fname_input.send_keys("sadjgdgjv")
+fname_input.send_keys("Abdelmounim")
 time.sleep(2)
 fname_input.clear()
 fname_input.send_keys(fname)
 
-
 # Enter name, last name, email
+print("last name - use locator")
+# driver.find_element(By.ID, 'lastName')
+driver.find_element(By.ID, 'lastName').send_keys(lname)
+time.sleep(5)
+
+print("Enter your email - use locator")
+# driver.find_element(By.CLASS_NAME, 'mr-sm-2 form-control').send_keys(email)
+driver.find_element(By.ID, 'userEmail').send_keys(email)
+time.sleep(5)
+
+
 # Select Gender: Male (radio)
 # Select/Enter data of birth
 # Select Checkboxes (multi select)
 # picture upload (input, type=file)
 # Select State first, then City (kind of drop down)
 
+print(" ************* Complete, closing the browser ************ ")
+driver.close() # close the current tab
+# driver.quit() # close the browser
 
