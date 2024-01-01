3926487149a905863faa82b12acfa377c17bcb5c
abdelmounimmassoum <abdel.massoum@gmail.com>
2023-12-31 19:23:03 -0500

    06-04-2023 class: update fixture scope

--- src/tests/test_sample_scenario1.py
+++ src/tests/test_sample_scenario1.py
@@ -5,13 +5,16 @@ print("Starting the sample test scenario.")
 
 @pytest.fixture(scope='module')
 def greet():
+    print("\n------------ SetUp -----------")
     print("Hellooooooooo Test Master!")
-    return [2,3,4]
+    yield [2,3,4]
+    print("\n------------ TearDown -----------")
+    print("\nFixture steps are completed here.")
 
 @pytest.mark.scen1
 @pytest.mark.scen1case1
 def test_scen1_case1(greet):
-    print("scenario 1 case 1 staring ..")
+    print("\nscenario 1 case 1 staring ..")
     print(greet)
     greet.append(5)
     print(greet)
@@ -22,7 +25,7 @@ def test_scen1_case1(greet):
 @pytest.mark.smoketest
 @pytest.mark.scen1case2
 def test_scen1_case2(greet):
-    print("scenario 1 case 2 staring ..")
+    print("\nscenario 1 case 2 staring ..")
     print(greet)
     greet.append(6)
     print(greet)
--- src/tests/test_sample_scenario2.py
+++ src/tests/test_sample_scenario2.py
@@ -3,13 +3,13 @@ import pytest
 @pytest.mark.smoketest
 @pytest.mark.scen2case1
 def test_scen2_case1():
-    print("scenario 2 case 1 starting ..")
+    print("\nscenario 2 case 1 starting ..")
     assert True
 
 
 @pytest.mark.scen2case2
 def test_scen2_case2():
-    print("scenario 2 case 2 starting ..")
+    print("\nscenario 2 case 2 starting ..")
     assert False
 
 
